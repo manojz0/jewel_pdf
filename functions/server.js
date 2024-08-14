a95509c86068b387e968748d8c6849dd19a844c7
manoj_ <manoj@managementseye.com>
2024-08-14 21:34:56 +0530

    changes

--- functions/server.js
+++ functions/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const serverless = require('serverless-http');
 
+const puppeteer = require('puppeteer');
+
 const app = express();
 
 app.get('/.netlify/functions/server', (req, res) => {
   res.send('Hello, World!');
+});
+
+
+app.get('/generate-pdf', (req, res) => {
+  res.send('generate-pdf, Hello, World!');
 });
 
 module.exports.handler = serverless(app);
